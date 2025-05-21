04dfbfe3e35b8bee65bb352207148ca1c42c4b9c
Hubert Lefevre <hubelefevre@gmail.com>
2025-05-21 21:56:33 +0200

    Basic CLI with completion generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+//! Goal: Reproduce my git prompt done in shell + python.
+//! The displayed information are:
+//! - Repo name: Either the origin URL path.
+//! - Which reference we are on
+//! - ongoing operation (if there is one)
+//! - ahead behind
+//! - schematic git status
+//! - schematic submodule status
+//!
+//! Custom Git status:
+//! + Add remotes list
+use clap::{Command, CommandFactory, Parser, Subcommand};
+use clap_complete::{generate, Generator, Shell};
+use std::io;
+
+#[derive(Parser, Debug, PartialEq)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the git repository to work with.
+    #[arg(short, long)]
+    repo: Option<String>,
+    /// Action to perform
+    #[command(subcommand)]
+    action: Action,
+}
+
+#[derive(Subcommand, Debug, PartialEq)]
+enum Action {
+    Prompt,
+    Status,
+    Completion { shell: Shell },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.action {
+        Action::Completion { shell } => {
+            generate_completion(&mut Args::command(), shell);
+        }
+        Action::Prompt => panic!("Not Implemented yet"),
+        Action::Status => panic!("Not Implemented yet"),
+    }
+}
+
+fn generate_completion<G: Generator + std::fmt::Debug>(
+    command: &mut Command,
+    generator: G,
+) {
+    eprintln!("Generating completion file for {generator:?}...");
+    generate(
+        generator,
+        command,
+        command.get_name().to_string(),
+        &mut io::stdout(),
+    );
 }
