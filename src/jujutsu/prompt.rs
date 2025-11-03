c72484d25bca90d784b18a4bf80f3766a10dfddc
Hubert Lefevre <hubelefevre@gmail.com>
2025-11-03 21:30:52 +0100

    Fix performance of jj prompt

--- src/jujutsu/prompt.rs
+++ src/jujutsu/prompt.rs
@@ -1,94 +1,28 @@
 use itertools::Itertools;
 use jj_lib::{
-    backend::BackendError,
-    commit::Commit,
-    op_store::LocalRemoteRefTarget,
-    ref_name::{RefName, WorkspaceName},
+    backend::{BackendResult, CommitId},
+    ref_name::WorkspaceName,
     repo::Repo,
-    store::Store,
-    view::View,
+    revset::RevsetExpression,
 };
 
 use super::load;
 use crate::cli::PromptBuilder;
 use colored::Colorize;
-use std::{path::Path, sync::Arc};
+use std::path::Path;
 
-struct CommitBookmark<'view> {
-    ref_name: &'view RefName,
-    // lrftp: LocalRemoteRefTarget<'view>,
-    distance: i32,
-}
-
-fn commit_has_parent_internal(
-    commit: &Commit,
-    ref_commit: &Commit,
-    mut distance: i32,
-) -> Result<Option<i32>, BackendError> {
-    if commit == ref_commit {
-        return Ok(Some(distance));
-    }
-
-    distance += 1;
-
-    for parent in commit.parents() {
-        let parent = parent?;
-        let res = commit_has_parent_internal(&parent, ref_commit, distance)?;
-        if res.is_some() {
-            return Ok(res);
-        }
-    }
-
-    Ok(None)
-}
-fn commit_has_parent(
-    commit: &Commit,
-    ref_commit: &Commit,
-) -> Result<Option<i32>, BackendError> {
-    commit_has_parent_internal(commit, ref_commit, 0)
-}
-
-impl<'view> CommitBookmark<'view> {
-    fn new(
-        repo_store: &Arc<Store>,
-        ref_name: &'view RefName,
-        lrftp: LocalRemoteRefTarget<'view>,
-        ref_commit: &Commit,
-    ) -> Result<Option<Self>, BackendError> {
-        Ok(lrftp
-            .local_target
-            .added_ids()
-            .map(|id| repo_store.get_commit(id))
-            .collect::<Result<Vec<Commit>, BackendError>>()?
-            .iter()
-            .filter_map(|c| commit_has_parent(c, ref_commit).transpose())
-            .collect::<Result<Vec<i32>, BackendError>>()?
-            .iter()
-            .max()
-            .map(|d| Self {
-                ref_name,
-                //l rftp,
-                distance: *d,
-            }))
-    }
-}
-
-fn get_commit_bookmarks<'view>(
-    repo_store: &Arc<Store>,
-    repo_view: &'view View,
-    commit: &Commit,
-) -> Result<Vec<CommitBookmark<'view>>, BackendError> {
-    repo_view
-        .bookmarks()
-        .filter_map(|(rf, lrft)| {
-            CommitBookmark::new(repo_store, rf, lrft, commit).transpose()
-        })
-        .collect()
+fn commit_descendants(
+    repo: &dyn Repo,
+    root: &CommitId,
+) -> BackendResult<Vec<CommitId>> {
+    let expr = RevsetExpression::commits(vec![root.clone()]).descendants();
+    let revset = expr.evaluate(repo).map_err(|e| e.into_backend_error())?;
+    let ids: Vec<CommitId> = revset.iter().map(|r| r.unwrap()).collect();
+    Ok(ids)
 }
 
 pub fn prompt(root: &Path, info: &mut PromptBuilder) -> i32 {
     let repo = load(root).unwrap();
-    let repo_store = repo.store();
     let repo_view = repo.view();
     let workspace_name = WorkspaceName::DEFAULT;
 
@@ -100,6 +34,24 @@ pub fn prompt(root: &Path, info: &mut PromptBuilder) -> i32 {
         None => return 1,
         Some(c) => c,
     };
+
+    let Ok(descendants) = commit_descendants(repo.as_ref(), commit.id())
+    else {
+        return 1;
+    };
+
+    let commit_bookmarks: Vec<&str> = repo_view
+        .bookmarks()
+        .filter_map(|(r, lrft)| {
+            if let Some(c) = lrft.local_target.as_normal()
+                && descendants.iter().contains(&c)
+            {
+                Some(r.as_str())
+            } else {
+                None
+            }
+        })
+        .collect();
 
     // Unneeded information in prompt. I know I am at @
     // let change_id = commit.change_id();
@@ -114,23 +66,20 @@ pub fn prompt(root: &Path, info: &mut PromptBuilder) -> i32 {
 
     let mut bookmarks = String::new();
     let mut print_other = false;
-    for commit_bookmark in get_commit_bookmarks(repo_store, repo_view, &commit)
-        .expect("Error retrieving bookmarks")
-        .iter()
-        .sorted_by_key(|cb| cb.distance)
-    {
+    for commit_bookmark in commit_bookmarks {
         bookmarks.push_str(&format!(
             "{}{}",
-            if commit_bookmark.distance == 0 {
-                "󰫍"
-            } else if print_other {
+            //if commit_bookmark.distance == 0 {
+            //   "󰫍"
+            //} else
+            if print_other {
                 "🞍"
             } else {
                 print_other = true;
                 "󰫎"
             }
             .bright_blue(),
-            commit_bookmark.ref_name.as_str().purple()
+            commit_bookmark.purple()
         ));
     }
     info.push_string(&if bookmarks.is_empty() {
