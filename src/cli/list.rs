08938ba5eb16865333ff4c7c30c003a2b55f0cbf
Hubert Lefevre <hubelefevre@gmail.com>
2026-03-03 09:50:05 +0100

    list: Add --host option
    Filter the listed repositories by host.

--- src/cli/list.rs
+++ src/cli/list.rs
@@ -1,6 +1,8 @@
+use clap_complete::engine::CompletionCandidate;
+
 use crate::{Config, UrlParser, get_repo_tree_dir, load_repo_tree};
 
-pub fn list() -> i32 {
+pub fn list(host: Option<String>) -> i32 {
     let repositories = load_repo_tree(
         &get_repo_tree_dir(),
         &UrlParser::new(&Config::default()),
@@ -8,7 +10,22 @@ pub fn list() -> i32 {
     .0;
 
     for repository in repositories {
+        if let Some(host) = &host {
+            if let Some(repo_host) = repository.id.host {
+                if &repo_host.name != host {
+                    continue;
+                }
+            } else if host != "local" {
+                continue;
+            }
+        }
         println!("{}", repository.root.display());
     }
     0
+}
+
+pub fn list_host_completer(
+    current: &std::ffi::OsStr,
+) -> Vec<CompletionCandidate> {
+    Config::default().host_completer(current)
 }
