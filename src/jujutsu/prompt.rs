1b4cec7bc92035d4b14743f7d44523f0254ea262
Hubert Lefevre <hubelefevre@gmail.com>
2026-03-11 23:10:55 +0100

    jj prompt: Review bookmark display
    Show when bookmark is modified. Show the three type of bookmarks I am
    interested to know relative to @.
    
    - Does the parent commit has a bookmark. I an then in the "git checkout"
      case. I did `jj new <bookmark>` and ended up on top of my branch,
      which would be the same result if I did `git checkout <branch>`.
      <bookmark> being equal to <branch>.
    - Does the current commit has a bookmark
    - Does any of the descendants commits have bookmark on which I will
      influence upon modifying current commits.
    
    Differ the different types of bookmarks:
    - local only
    - tracked
    - untracked

--- src/jujutsu/prompt.rs
+++ src/jujutsu/prompt.rs
@@ -1,10 +1,10 @@
 use std::path::Path;
 
 use colored::Colorize;
-use itertools::Itertools;
 use jj_lib::{
     backend::{BackendResult, CommitId},
-    ref_name::WorkspaceName,
+    op_store::LocalRemoteRefTarget,
+    ref_name::{RefName, WorkspaceName},
     repo::Repo,
     revset::RevsetExpression,
 };
@@ -12,70 +12,182 @@ use jj_lib::{
 use super::load;
 use crate::cli::PromptBuilder;
 
-fn commit_descendants(
+#[derive(Debug)]
+struct Ref {
+    name: String,
+    modified: bool,
+    local_only: bool,
+    remote_only: Option<String>,
+}
+
+impl Ref {
+    fn try_new(
+        name: &RefName,
+        lrrt: &LocalRemoteRefTarget,
+        current_commit: &CommitId,
+    ) -> Option<Self> {
+        let local_target = lrrt.local_target.as_normal();
+        let local_only = lrrt.remote_refs.is_empty();
+        let remote_only = if let Some(local_target) = local_target {
+            (local_target == current_commit).then_some(None)?
+        } else {
+            Some(lrrt.remote_refs.iter().find_map(
+                |&(remote_name, remote_ref)| {
+                    remote_ref.target.as_normal().map(|c| {
+                        (c == current_commit)
+                            .then_some(remote_name.as_str().to_string())
+                    })
+                },
+            )??)
+        };
+
+        let modified = local_target.is_some_and(|l| {
+            lrrt.remote_refs.iter().any(|&(_, remote_ref)| {
+                remote_ref.target.as_normal().is_some_and(|c| c != l)
+            })
+        });
+
+        Some(Self {
+            name: name.as_str().to_string(),
+            modified,
+            local_only,
+            remote_only,
+        })
+    }
+
+    fn get_bookmark_repr(&self) -> String {
+        if let Some(remote) = &self.remote_only {
+            format!("{}@{}", self.name.as_str(), remote).purple()
+        } else if self.local_only {
+            self.name.as_str().bright_green()
+        } else {
+            format!(
+                "{}{}",
+                self.name.as_str(),
+                if self.modified { "*" } else { "" }
+            )
+            .bright_purple()
+        }
+        .to_string()
+    }
+}
+
+enum BookmarkCategory {
+    /// The bookmark is set at the current commit .
+    Current,
+    /// The bookmark is set to the direct parent of the current commit.
+    Parents,
+    /// The bookmark is set to a commit which is a descendant of the current
+    /// commit.
+    Descendants,
+}
+
+impl BookmarkCategory {
+    fn get_bookmarks(
+        &self,
+        repo: &dyn Repo,
+        current_commit: &CommitId,
+    ) -> BackendResult<impl Iterator<Item = Ref>> {
+        let revset = RevsetExpression::commits(vec![current_commit.to_owned()]);
+
+        Ok(match self {
+            Self::Current => revset,
+            Self::Parents => revset.parents(),
+            Self::Descendants => revset.descendants_at(1).descendants(),
+        }
+        .evaluate(repo)
+        .map_err(|e| e.into_backend_error())?
+        .iter()
+        .flat_map(|r| {
+            let commit = r.unwrap();
+            repo.view().bookmarks().filter_map(move |(name, lrrt)| {
+                Ref::try_new(name, &lrrt, &commit)
+            })
+        }))
+    }
+
+    fn get_repr(&self) -> String {
+        match self {
+            Self::Current => "󰫍".bright_blue(),
+            Self::Parents => "󰫍".yellow(),
+            Self::Descendants => "󰫎".bright_blue(),
+        }
+        .to_string()
+    }
+}
+
+fn list_bookmarks(
+    category: BookmarkCategory,
     repo: &dyn Repo,
-    root: &CommitId,
-) -> BackendResult<Vec<CommitId>> {
-    let expr = RevsetExpression::commits(vec![root.clone()]).descendants();
-    let revset = expr.evaluate(repo).map_err(|e| e.into_backend_error())?;
-    let ids: Vec<CommitId> = revset.iter().map(|r| r.unwrap()).collect();
-    Ok(ids)
+    current_commit: &CommitId,
+    buffer: &mut String,
+) -> BackendResult<()> {
+    let mut bookmarks = category.get_bookmarks(repo, current_commit)?;
+
+    if let Some(bookmark) = bookmarks.next() {
+        if !buffer.is_empty() {
+            buffer.push(' ');
+        }
+
+        buffer.push_str(&category.get_repr());
+        buffer.push_str(&bookmark.get_bookmark_repr());
+
+        for bookmark in bookmarks {
+            buffer.push_str(&"🞍".bright_blue());
+            buffer.push_str(&bookmark.get_bookmark_repr());
+        }
+    }
+
+    Ok(())
+}
+
+fn prompt_internal(
+    repo: &dyn Repo,
+    current_commit: &CommitId,
+    info: &mut PromptBuilder,
+) -> BackendResult<()> {
+    let mut buffer = String::new();
+
+    list_bookmarks(
+        BookmarkCategory::Parents,
+        repo,
+        current_commit,
+        &mut buffer,
+    )?;
+    list_bookmarks(
+        BookmarkCategory::Current,
+        repo,
+        current_commit,
+        &mut buffer,
+    )?;
+    list_bookmarks(
+        BookmarkCategory::Descendants,
+        repo,
+        current_commit,
+        &mut buffer,
+    )?;
+
+    info.push_string(&if buffer.is_empty() {
+        "󰫌".bright_black().to_string()
+    } else {
+        buffer
+    });
+
+    Ok(())
 }
 
 pub fn prompt(root: &Path, info: &mut PromptBuilder) -> i32 {
     let repo = load(root).unwrap();
-    let repo_view = repo.view();
-    let workspace_name = WorkspaceName::DEFAULT;
-
-    let commit = match repo
-        .view()
-        .get_wc_commit_id(workspace_name)
-        .and_then(|id| repo.store().get_commit(id).ok())
-    {
-        None => return 1,
-        Some(c) => c,
-    };
-
-    let Ok(descendants) = commit_descendants(repo.as_ref(), commit.id()) else {
+    let Some(current_commit) =
+        repo.view().get_wc_commit_id(WorkspaceName::DEFAULT)
+    else {
         return 1;
     };
 
-    let commit_bookmarks: Vec<&str> = repo_view
-        .bookmarks()
-        .filter_map(|(r, lrft)| {
-            if let Some(c) = lrft.local_target.as_normal()
-                && descendants.iter().contains(&c)
-            {
-                Some(r.as_str())
-            } else {
-                None
-            }
-        })
-        .collect();
-
-    let mut bookmarks = String::new();
-    let mut print_other = false;
-    for commit_bookmark in commit_bookmarks {
-        bookmarks.push_str(&format!(
-            "{}{}",
-            //if commit_bookmark.distance == 0 {
-            //   "󰫍"
-            //} else
-            if print_other {
-                "🞍"
-            } else {
-                print_other = true;
-                "󰫎"
-            }
-            .bright_blue(),
-            commit_bookmark.purple()
-        ));
-    }
-    info.push_string(&if bookmarks.is_empty() {
-        "󰫌".bright_black().to_string()
+    if let Err(err) = prompt_internal(repo.as_ref(), current_commit, info) {
+        eprintln!("{err}");
+        1
     } else {
-        bookmarks
-    });
-
-    0
+        0
+    }
 }
