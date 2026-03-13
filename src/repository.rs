ef1548f59936eea56ffc18c696977e7e961062d7
Hubert Lefevre <hubelefevre@gmail.com>
2026-03-13 22:02:24 +0100

    Define RepoState
    Structure to contain the state of a repository.

--- src/repository.rs
+++ src/repository.rs
@@ -5,6 +5,8 @@ use std::{
     path::{Path, PathBuf},
 };
 
+use colored::Colorize;
+
 use crate::{
     UrlParser,
     config::Host,
@@ -182,4 +184,49 @@ pub fn search(
     url_parser: &UrlParser,
 ) -> (Vec<Repository>, Vec<PathBuf>) {
     _search(repo_tree_dir, repo_tree_dir, url_parser)
+}
+
+pub struct RepoState {
+    pub unpushed_commits: bool,
+    pub needs_restack: bool,
+    pub has_conflicts: bool,
+}
+
+impl RepoState {
+    #[expect(unused)]
+    pub fn is_ok(&self) -> bool {
+        !(self.unpushed_commits || self.needs_restack || self.has_conflicts)
+    }
+}
+
+impl Display for RepoState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut empty = true;
+        if self.unpushed_commits {
+            write!(f, "{}", "unpushed commits".purple())?;
+            empty = false;
+        }
+
+        if self.needs_restack {
+            if !empty {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}", "needs restack".yellow())?;
+            empty = false;
+        }
+
+        if self.has_conflicts {
+            if !empty {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}", "has_conflicts".bright_red())?;
+            empty = false;
+        }
+
+        if empty {
+            write!(f, "{}", "OK".green())?;
+        }
+
+        Ok(())
+    }
 }
