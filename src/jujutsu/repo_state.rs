2512b2a3e2e16b5d3f28b05f449e5d9cea213b2b
Hubert Lefevre <hubelefevre@gmail.com>
2026-03-18 13:05:55 +0100

    WIP repo state: Detect when some bookmarks have been deleted
    And those are pending to be deleted to the remote.

--- src/jujutsu/repo_state.rs
+++ src/jujutsu/repo_state.rs
@@ -9,6 +9,9 @@
 use std::error::Error;
 use std::path::Path;
 
+use jj_lib::repo::Repo;
+
+use super::load;
 use super::revsets;
 use crate::repo_state::RepoState;
 
@@ -36,13 +39,23 @@ pub fn has_conflicts(repo_path: &Path) -> Result<bool, Box<dyn Error>> {
     revsets::revset_has_match(repo_path, "conflicts()")
 }
 
+/// Find out if the repository has reference pending to be deleted.
+fn has_pending_ref_to_delete(repo: &dyn Repo) -> Result<bool, Box<dyn Error>> {
+    Ok(repo
+        .view()
+        .local_bookmarks()
+        .any(|(name, _)| repo.view().bookmarks().all(|(n, _)| n != name)))
+}
+
 /// Get the repository state as RepoState struct.
 pub async fn get_repo_state(
     repo_path: &Path,
 ) -> Result<RepoState, Box<dyn Error>> {
+    let repo = load(repo_path).await?;
     Ok(RepoState::new(
         has_unpushed_commits(repo_path)?,
         needs_restack(repo_path)?,
         has_conflicts(repo_path)?,
+        has_pending_ref_to_delete(repo.as_ref())?,
     ))
 }
