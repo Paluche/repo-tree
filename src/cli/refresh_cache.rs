d257ae2a65ee0cfd8ec96c6021defc34f3ebb6c9
Hubert Lefevre <hubelefevre@gmail.com>
2026-04-07 23:33:52 +0200

    Create `rt refresh-cache`
    Action to refresh the repositories cache and do nothing else.

---
+++ src/cli/refresh_cache.rs
@@ -0,0 +1,15 @@
+//! Refresh the repositories cache.
+use clap::Args;
+
+use crate::config::Config;
+use crate::repository::Repositories;
+
+/// Refresh the repositories cache.
+#[derive(Args, Debug, PartialEq)]
+pub struct RefreshCacheArgs {}
+
+/// Execute the `rt refresh-cache` command.
+pub fn run(config: &Config, _: RefreshCacheArgs) -> i32 {
+    Repositories::load(config, true);
+    0
+}
