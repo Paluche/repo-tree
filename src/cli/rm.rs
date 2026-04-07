93989a13177a4ecbc1d8ce094b42ccdd161e0e33
Hubert Lefevre <hubelefevre@gmail.com>
2026-04-07 12:58:26 +0200

    cli::rm: Clean
    Remove one non needed indentation level.

--- src/cli/rm.rs
+++ src/cli/rm.rs
@@ -24,63 +24,64 @@ pub struct RmArgs {
 /// Execute the `rt rm` command.
 pub fn run(config: &Config, args: RmArgs) -> i32 {
     let repositories = Repositories::load(config);
-    if let Some(repository) = match resolve(config, &repositories, args.repo_id)
-    {
-        Ok(v) => v,
+    let repository = match resolve(config, &repositories, args.repo_id) {
+        Ok(v) => match v {
+            Some(repo) => repo,
+            None => {
+                eprintln!("No repository found matching the given identifier");
+                return 2;
+            }
+        },
         Err(err) => {
             eprintln!("{err}");
             return 1;
         }
-    } {
-        match &repository.state() {
-            Ok(repo_state) => {
-                if repo_state.has_unpushed_commits() {
-                    eprintln!("WARNING: The repository has unpushed commits");
-                }
-            }
-            Err(err) => {
-                if err.downcast_ref::<NotImplementedError>().is_some() {
-                    eprintln!(
-                        "Unable to check if the repository has unpushed \
-                         commits"
-                    );
-                } else {
-                    eprintln!("{err}");
-                    return 1;
-                }
+    };
+
+    match &repository.state() {
+        Ok(repo_state) => {
+            if repo_state.has_unpushed_commits() {
+                eprintln!("WARNING: The repository has unpushed commits");
             }
         }
-
-        if !args.force {
-            // Ask the user for confirmation before removing the repository.
-            println!(
-                "Are you sure you want to remove the repository {}? [y/N]",
-                repository.root.display()
-            );
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input).unwrap();
-            if input.trim().to_lowercase() != "y" {
-                println!("Aborting.");
+        Err(err) => {
+            if err.downcast_ref::<NotImplementedError>().is_some() {
+                eprintln!(
+                    "Unable to check if the repository has unpushed commits"
+                );
+            } else {
+                eprintln!("{err}");
                 return 1;
             }
         }
-
-        // Remove the repository from the repo tree.
-        remove_dir_all(&repository.root)
-            .expect("Failed to remove the repository");
-
-        // Remove parent directories if they are empty.
-        let parent = &repository.root;
-        while let Some(parent) = parent.parent() {
-            if parent.read_dir().unwrap().next().is_none() {
-                std::fs::remove_dir(parent).unwrap();
-            } else {
-                break;
-            }
-        }
-
-        0
-    } else {
-        2
     }
+
+    if !args.force {
+        // Ask the user for confirmation before removing the repository.
+        println!(
+            "Are you sure you want to remove the repository {}? [y/N]",
+            repository.root.display()
+        );
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        if input.trim().to_lowercase() != "y" {
+            println!("Aborting.");
+            return 1;
+        }
+    }
+
+    // Remove the repository from the repo tree.
+    remove_dir_all(&repository.root).expect("Failed to remove the repository");
+
+    // Remove parent directories if they are empty.
+    let parent = &repository.root;
+    while let Some(parent) = parent.parent() {
+        if parent.read_dir().unwrap().next().is_none() {
+            std::fs::remove_dir(parent).unwrap();
+        } else {
+            break;
+        }
+    }
+
+    0
 }
