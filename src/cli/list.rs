d8412bf4b84f6224ed9f53cc862634a667d80b4d
Hubert Lefevre <hubelefevre@gmail.com>
2026-05-26 13:51:09 +0200

    Fix help text for host and name filter arguments
    We change the type of input, but did not adapt the help text.

--- src/cli/list.rs
+++ src/cli/list.rs
@@ -12,18 +12,19 @@ use crate::repository::Repositories;
 #[derive(Args)]
 pub struct ListArgs {
     /// Filter the repositories to list by their host. For example, "github" or
-    /// "local". Can be specified multiple times.
+    /// "local". You can specify glob patterns. Can be specified multiple times
+    /// as an union filter.
     #[arg(
         short='H', long="host", action=ArgAction::Append,
         add=ArgValueCompleter::new(list_host_completer)
         )
     ]
     hosts: Vec<Glob>,
-    /// Filter the repositories to by their name, within its forge. All
-    /// repositories which name starts with the provided value will be
-    /// listed. For example to filter only GitHub repositories from a
-    /// certain organization, you could use the organization name as value
-    /// for this argument, and "github" as value of the --host argument.
+    /// Filter the repositories to by their name. You can specify glob
+    /// patterns. For example to filter only GitHub repositories from a
+    /// certain organization (e.g. 'owner'), you could use the 'owner/*' as
+    /// value for this argument, and "github" as value of the --host
+    /// argument. Can be specified multiple times as an union filter.
     #[arg(short = 'N', long = "name", action=ArgAction::Append)]
     names: Vec<Glob>,
     /// Force recreating the cache.
